9a4ea6ff82b27d617e5f84e84ba2f2f7be88ed58
Arian Sani <ariansani@hotmail.com>
2022-09-07 12:11:00 +0800

    formarray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TodoComponent } from './components/todo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild('todo')
+  todoComp!: TodoComponent
+
   title = 'day32';
+
+  value: number = 3
+  numList: number[] = []
+
+  ngOnInit():void{
+    for (let index = 0; index < 5; index++) {
+      this.numList.push(Math.floor(Math.random()*100))
+      
+    }
+  }
+
+  saveTodo(){
+
+    const todo = this.todoComp.getValues()
+    console.info(">>>>>saveTodo: ", todo)
+  }
+
+
+
+
+
+  randomize(){
+    this.value = Math.floor(Math.random() * 10)
+
+    //Create new array
+    const tmp:number[] = [...this.numList, this.value]
+
+    //assign the new array to the binding
+    this.numList = tmp
+  }
+
+
+
 }
